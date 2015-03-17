3dcde7a9e78e3c6ef8762e80a4afff6cad8f94fc
mkrier02 <mark@junto-solutions.com>
2015-03-17 18:27:11 -0400

    Changed DB from mysql to sqlite3

--- djangoapp/settings.py
+++ djangoapp/settings.py
@@ -60,8 +60,8 @@ import os
  
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ['RDS_DB_NAME'],
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.environ[BASE_DIR,'RDS_DB_NAME'],
         'USER': os.environ['RDS_USERNAME'],
         'PASSWORD': os.environ['RDS_PASSWORD'],
         'HOST': os.environ['RDS_HOSTNAME'],
